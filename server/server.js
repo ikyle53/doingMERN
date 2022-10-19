7e0b48c6496c68821b16b9a5e4a432eead9459b8
Kyle H <ikyle53@icloud.com>
2022-10-19 09:19:40 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+
+// get driver connection
+const dbo = request("./db/conn");
+
+app.listen(port, () => {
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    });
+    console.log('Server is running on port:${port}');
+});
